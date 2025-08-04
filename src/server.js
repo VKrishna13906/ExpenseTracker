0adf463385cbc8f2887f546ff19fcf3b73a7d8f2
VKrishna13906 <krishnavishwakarma13906@gmail.com>
2025-08-04 15:35:59 +0530

    Added cors for error resolution

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { initDb } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionRoute from "./route/transactionRoute.js";
+import cors from "cors"; // ✅
+
 
 dotenv.config();
 
@@ -12,6 +14,10 @@ const PORT = parseInt(process.env.PORT, 10) || 5002;
 //middleware
 app.use(rateLimiter);
 app.use(express.json());
+app.use(cors({
+  origin: "*", // or your frontend IP
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 //middleware
 /* here basically we are creating a folder structure same as api, so when the url contains the transact it will route to transactionRoute, infuture if i have to create more requrest but not related to transaction i can addd more request same as this 
